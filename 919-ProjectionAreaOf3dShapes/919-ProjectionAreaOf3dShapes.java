3670bd0f1d3abc0d4fea225281a15acce3893e31
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:48:52 +0530

    Create 919-ProjectionAreaOf3dShapes

---
+++ 919-ProjectionAreaOf3dShapes/919-ProjectionAreaOf3dShapes.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:48:51 AM
+class Solution {
+    public int projectionArea(int[][] grid) {
+        int a=0,x=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            int mr=Integer.MIN_VALUE;
+            int mc=Integer.MIN_VALUE;
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]!=0)x+=1;
+                if(grid[i][j]>mr)mr=grid[i][j];
+                if(grid[j][i]>mc)mc=grid[j][i];
+            }
+            a+=mr+mc;
+        }
+        return (a+x);
+    }
+}
