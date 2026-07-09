edd328b9f9a827646413be1223c709026ef0f304
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:47:38 +0530

    Create 1458-SortIntegersByTheNumberOf1Bits

---
+++ 1458-SortIntegersByTheNumberOf1Bits/1458-SortIntegersByTheNumberOf1Bits.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:47:37 AM
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] temp = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        
+        Arrays.sort(temp, (a, b) -> {
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+            
+            if (countA == countB) {
+                return a - b;
+            }
+            return countA - countB;
+        });
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+        
+        return arr;
+    }
+}
