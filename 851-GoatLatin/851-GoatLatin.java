53fa47ff139a4db44c3d008b808a643e64b9e2bb
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:49:04 +0530

    Create 851-GoatLatin

---
+++ 851-GoatLatin/851-GoatLatin.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:49:04 AM
+class Solution {
+    public String toGoatLatin(String sentence) {
+        String [] str=sentence.split(" ");
+        String vowel="AEIOUaeiou";
+        String[] res=new String[str.length];
+        for(int i=0;i<str.length;i++){
+            StringBuilder stb=new StringBuilder();
+            String strs=str[i];
+            if(vowel.indexOf(strs.charAt(0))!=-1){
+                stb.append(strs);
+            }
+            else{
+                stb.append(strs.substring(1));
+                stb.append(strs.charAt(0));
+            }
+            stb.append("ma");
+            for(int j=0;j<=i;j++){
+                stb.append('a');
+            }
+            res[i]=stb.toString();
+            
+        }
+        return String.join(" ",res);
+        }
+    }
