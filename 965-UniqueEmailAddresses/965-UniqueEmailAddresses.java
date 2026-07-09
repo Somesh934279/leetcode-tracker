eece5f8f96212f5b218eb21af4b34c65927c5760
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:48:36 +0530

    Create 965-UniqueEmailAddresses

---
+++ 965-UniqueEmailAddresses/965-UniqueEmailAddresses.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:48:36 AM
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet();
+        int count=0;
+        for(String email: emails){
+            int atIndex = email.indexOf("@");
+            int plusIndex= email.indexOf("+");
+            String str1 = "";
+            if(plusIndex>=0) str1 = email.substring(0,plusIndex);
+            else str1 = email.substring(0,atIndex);
+            str1 = str1.replace(".","") + email.substring(atIndex);
+            if(set.add(str1)) count++;
+        }
+        return count;
+    }
+}
