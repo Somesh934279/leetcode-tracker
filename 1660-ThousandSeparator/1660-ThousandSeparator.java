10275d74d97f7ef4d1f5fe8d223a42c13e632753
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:47:10 +0530

    Create 1660-ThousandSeparator

---
+++ 1660-ThousandSeparator/1660-ThousandSeparator.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 9:47:09 AM
+class Solution {
+    public String thousandSeparator(int n) {
+        String s = Integer.toString(n);
+
+        if (s.length() <= 3) {
+            return s;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+            count++;
+
+            if (count == 3 && i > 0) {
+                sb.append('.');
+                count = 0;
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+}
