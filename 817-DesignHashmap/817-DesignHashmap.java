c2cd52af1525b4e184fb6b2c32000ecc5008c521
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:49:14 +0530

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:49:13 AM
+class MyHashMap {
+    int[] data;
+    public MyHashMap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+    public int get(int key) {
+        return data[key];
+    }
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
