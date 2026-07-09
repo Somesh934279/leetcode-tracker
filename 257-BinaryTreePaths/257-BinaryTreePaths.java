445fc80fa8c152246a530f259fa43743a7550c59
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:50:50 +0530

    Create 257-BinaryTreePaths

---
+++ 257-BinaryTreePaths/257-BinaryTreePaths.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:50:50 AM
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        dfs(root, "", result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> result) {
+        if (node == null) return;
+      
+        path += node.val;
+
+       
+        if (node.left == null && node.right == null) {
+            result.add(path);
+        } else {
+            path += "->";
+            dfs(node.left, path, result);
+            dfs(node.right, path, result);
+        }
+    }
+}
