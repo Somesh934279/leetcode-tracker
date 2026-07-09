0446b6d09a29bbbd12f6c8bcd4267dacea096b70
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:50:10 +0530

    Create 500-KeyboardRow

---
+++ 500-KeyboardRow/500-KeyboardRow.java
@@ -0,0 +1,34 @@
+// Last updated: 7/9/2026, 9:50:10 AM
+class Solution {
+    public String[] findWords(String[] arr) {
+        String s1 = "qwertyuiop";
+        String s2 = "asdfghjkl";
+        String s3 = "zxcvbnm";
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            String[] a = arr[i].toLowerCase().split("");
+            boolean flag = true;
+            String temp = "";
+
+            if (s1.contains(a[0]))
+                temp = s1;
+            else if (s2.contains(a[0]))
+                temp = s2;
+            else 
+                temp = s3;
+            
+            for (int j = 1; j < a.length; j++) 
+                if (!temp.contains(a[j])) {
+                    flag = false;
+                    break;
+                }
+            
+            if (flag) 
+                list.add(arr[i]);
+        }
+
+        return list.toArray(new String[list.size()]);
+    }
+}
