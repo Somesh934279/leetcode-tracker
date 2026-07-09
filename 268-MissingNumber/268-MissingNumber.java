a939a52741df2cd50350231cb97063ff61625670
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:50:49 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 9:50:48 AM
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+    
+        return total - sum;
+    }
+}
