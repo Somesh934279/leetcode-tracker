45536d5aebd428acc82244c11092bed882f69def
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:52:34 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:52:34 AM
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Overflow check
+            if (rev > Integer.MAX_VALUE / 10 || 
+                rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return rev;
+    }
+}
