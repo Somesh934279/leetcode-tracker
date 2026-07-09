e573bbeeee08d1ee911edb7bd9e1ca24bc0e7762
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:50:35 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:50:34 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> dictionary = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);   
+            if (!dictionary.containsKey(c)) {
+                dictionary.put(c, 1);
+            } else {
+               
+                dictionary.put(c, dictionary.get(c) + 1);
+            }
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);   
+            if (dictionary.containsKey(c) && dictionary.get(c) > 0) {
+                dictionary.put(c, dictionary.get(c) - 1);
+            } else {   
+                return false;
+            }
+        }
+        return true;
+    }
+}
