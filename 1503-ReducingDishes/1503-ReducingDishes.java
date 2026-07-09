23c97870b2e1814bef61fce0217dcc4b119da48c
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:47:32 +0530

    Create 1503-ReducingDishes

---
+++ 1503-ReducingDishes/1503-ReducingDishes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:47:31 AM
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int n = satisfaction.length;
+        int presum = 0, res = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            presum += satisfaction[i];
+            if (presum < 0) {
+                break;
+            }
+            res += presum;
+        }
+        return res;
+    }
+}
