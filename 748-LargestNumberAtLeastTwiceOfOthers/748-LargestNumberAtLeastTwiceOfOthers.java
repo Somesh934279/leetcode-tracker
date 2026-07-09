6998fe1f50e7eb4e4f3f2ae5b337b15f04c84d7f
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:49:23 +0530

    Create 748-LargestNumberAtLeastTwiceOfOthers

---
+++ 748-LargestNumberAtLeastTwiceOfOthers/748-LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:49:22 AM
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int idx = -1;;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+                idx = i;
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(i != idx && nums[i] * 2 > max){
+                return -1;
+            }
+        }
+        return idx;
+    }
+}
