8018809230c338ab33c64f637db98fc022e6e2f6
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:47:16 +0530

    Create 1580-ShuffleTheArray

---
+++ 1580-ShuffleTheArray/1580-ShuffleTheArray.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 9:47:15 AM
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] r = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            r[2 * i] = nums[i];
+            r[2 * i + 1] = nums[n + i];
+        }
+
+        return r;
+    }
+}
