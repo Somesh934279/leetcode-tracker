899858ff8d714d3d1e5f641143faeceae571af22
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:51:43 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:51:43 AM
+class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
+    }
+}
