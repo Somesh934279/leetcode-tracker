291ea2759dd1fc42d75839b02362e9c354d7bbf8
Somesh934279 <23cs105@drngpit.ac.in>
2026-07-09 09:49:27 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:49:27 AM
+public class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        
+        Solution solution = new Solution();
+        System.out.println(solution.toLowerCase("Hello"));  
+        System.out.println(solution.toLowerCase("here"));  
+        System.out.println(solution.toLowerCase("LOVELY")); 
+    }
+}
